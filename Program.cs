69282de038594d4facf8653235d62f5e0c246df7
warrior14 <ironaddicts14@gmail.com>
2021-08-26 10:24:18 -0500

    finished the program.cs portion

--- Program.cs
+++ Program.cs
@@ -8,9 +8,22 @@ namespace VillianGang
         static void Main(string[] args)
         {
             
-        Evil
+        // passing parameter to constructor    
+        Evil venom = new Evil("Venom");
+        Console.WriteLine($"Named Evil: {venom.Name}");
 
 
+        // call a method on the class instance 
+        venom.Reassemble();
+
+
+        // using the object initializer 
+        Evil namedWithInitializerEvil = new Evil() { Name = "Green Goblin"};
+        Console.WriteLine($"Initializer Evil: {namedWithInitializerEvil.Name}");
+
+        // relying on the default name in the overload of the constructor with no params
+        Evil unknownEvil = new Evil();
+        Console.WriteLine($"Unknown Evil: {unknownEvil.Name}");
 
         }
     }
