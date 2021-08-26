4a9fb586035fa130105b436cdb22a947397b25b2
warrior14 <ironaddicts14@gmail.com>
2021-08-26 09:23:25 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VillianGang
+{
+    class Program
+    {
+        // args is an array of strings passed from the console when the program starts
+        static void Main(string[] args)
+        {
+            
+        Evil
+
+
+
+        }
+    }
+}
